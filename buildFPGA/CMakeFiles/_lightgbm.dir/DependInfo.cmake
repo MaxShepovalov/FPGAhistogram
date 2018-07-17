ef16c4fab5faa43d9868aebbe3bbf5b2ee0cac57
Cloud User <centos@ip-172-31-26-240.us-west-2.compute.internal>
2018-07-17 20:45:47 +0000

    GPU tests

---
+++ buildFPGA/CMakeFiles/_lightgbm.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/centos/gbm/LightGBM/src/application/application.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/application/application.cpp.o"
+  "/home/centos/gbm/LightGBM/src/boosting/boosting.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/boosting/boosting.cpp.o"
+  "/home/centos/gbm/LightGBM/src/boosting/gbdt.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/boosting/gbdt.cpp.o"
+  "/home/centos/gbm/LightGBM/src/boosting/gbdt_model_text.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/boosting/gbdt_model_text.cpp.o"
+  "/home/centos/gbm/LightGBM/src/boosting/gbdt_prediction.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/boosting/gbdt_prediction.cpp.o"
+  "/home/centos/gbm/LightGBM/src/boosting/prediction_early_stop.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/boosting/prediction_early_stop.cpp.o"
+  "/home/centos/gbm/LightGBM/src/c_api.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/c_api.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/bin.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/bin.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/config.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/config.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/dataset.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/dataset.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/dataset_loader.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/dataset_loader.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/file_io.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/file_io.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/json11.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/json11.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/metadata.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/metadata.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/parser.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/parser.cpp.o"
+  "/home/centos/gbm/LightGBM/src/io/tree.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/io/tree.cpp.o"
+  "/home/centos/gbm/LightGBM/src/lightgbm_R.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/lightgbm_R.cpp.o"
+  "/home/centos/gbm/LightGBM/src/metric/dcg_calculator.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/metric/dcg_calculator.cpp.o"
+  "/home/centos/gbm/LightGBM/src/metric/metric.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/metric/metric.cpp.o"
+  "/home/centos/gbm/LightGBM/src/network/linker_topo.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/network/linker_topo.cpp.o"
+  "/home/centos/gbm/LightGBM/src/network/linkers_mpi.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/network/linkers_mpi.cpp.o"
+  "/home/centos/gbm/LightGBM/src/network/linkers_socket.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/network/linkers_socket.cpp.o"
+  "/home/centos/gbm/LightGBM/src/network/network.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/network/network.cpp.o"
+  "/home/centos/gbm/LightGBM/src/objective/objective_function.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/objective/objective_function.cpp.o"
+  "/home/centos/gbm/LightGBM/src/treelearner/data_parallel_tree_learner.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/treelearner/data_parallel_tree_learner.cpp.o"
+  "/home/centos/gbm/LightGBM/src/treelearner/feature_parallel_tree_learner.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/treelearner/feature_parallel_tree_learner.cpp.o"
+  "/home/centos/gbm/LightGBM/src/treelearner/gpu_tree_learner.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/treelearner/gpu_tree_learner.cpp.o"
+  "/home/centos/gbm/LightGBM/src/treelearner/serial_tree_learner.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/treelearner/serial_tree_learner.cpp.o"
+  "/home/centos/gbm/LightGBM/src/treelearner/tree_learner.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/treelearner/tree_learner.cpp.o"
+  "/home/centos/gbm/LightGBM/src/treelearner/voting_parallel_tree_learner.cpp" "/home/centos/gbm/LightGBM/buildFPGA/CMakeFiles/_lightgbm.dir/src/treelearner/voting_parallel_tree_learner.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_SOCKET"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
